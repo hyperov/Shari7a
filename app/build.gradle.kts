cd3884fb27b8f075970a74a428340afbd1c9d80e
Ahmed Nabil <a.ahmed.nabil90@gmail.com>
2026-05-20 10:08:43 +0300

    increase version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         applicationId = "com.nabil.ahmed.shari7a"
         minSdk = 28
         targetSdk = 37
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
